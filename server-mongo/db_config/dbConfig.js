baa6c248bf1fac10c53d8aca60f6b99df999b216
Menelaos <mkaskiris@hotmail.com>
2022-01-18 18:20:57 +0000

    changed dbconfig

--- server-mongo/db_config/dbConfig.js
+++ server-mongo/db_config/dbConfig.js
@@ -3,9 +3,9 @@ const connectionUrl = process.env.DB_CONNECTION;
 const dbName = process.env.DB_NAME
 
 const init = async () => {
-  let client = await MongoClient.connect(connectionUrl)
+  let client = await MongoClient.connect("mongodb+srv://quiz_user:quiz1234@quiz-app-ireland.58s8z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
   //console.log('connected to database!', dbName)
-  return client.db(dbName)
+  return client.db('users')
 }
 
 module.exports = { init };
