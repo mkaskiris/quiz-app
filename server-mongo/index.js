5d2d8c8e2fdeefb73f9091015439466e6fd5972e
Menelaos <mkaskiris@hotmail.com>
2022-01-18 17:57:12 +0000

    npm start

--- server-mongo/index.js
+++ server-mongo/index.js
@@ -1,4 +1,4 @@
 const app = require('./server');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Express just departed from port ${port}!`))
